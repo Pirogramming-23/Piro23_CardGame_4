4677da1fcaa0f45994614db8a685975157995118
JooKyung-Yuh <johnlu0410@gmail.com>
2025-07-19 06:32:04 +0900

    [fix] models.py 수정

--- card_4/settings_production.py
+++ card_4/settings_production.py
@@ -42,7 +42,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Static files serving
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
