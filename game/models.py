d65d68963c951304cd4d821f2570a4fb22f95f3b
JooKyung-Yuh <johnlu0410@gmail.com>
2025-07-19 05:05:04 +0900

    [fix] CustomUser game 확인

--- game/models.py
+++ game/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
 
 
 # 커스텀 유저 모델
@@ -58,3 +61,8 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}의 프로필"
+
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
