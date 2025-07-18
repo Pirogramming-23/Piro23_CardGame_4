8fcb274373f73f9f32cc9d5cb20bac873b632573
JooKyung-Yuh <johnlu0410@gmail.com>
2025-07-19 06:19:14 +0900

    [fix] env 파일 삭제

--- card_4/settings_production.py
+++ card_4/settings_production.py
@@ -42,7 +42,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Static files serving
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
